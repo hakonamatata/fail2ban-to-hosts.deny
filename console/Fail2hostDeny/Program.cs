b1230ce2fc857b2e2fb50963538827d55e5015b0
Jan Sviland <jan@sviland.org>
2019-01-04 16:06:14 +0100

    console project c#

---
+++ console/Fail2hostDeny/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fail2hostDeny
+{
+    class Program
+    {
+        private const string usageText = "Usage: fail2hostDeny.exe -i fail2ban.log -o host.deny";
+
+        static int Main(String[] args)
+        {
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine(usageText);
+                return 1;
+            }
+
+            return 0;
+
+        }
+    }
+}
