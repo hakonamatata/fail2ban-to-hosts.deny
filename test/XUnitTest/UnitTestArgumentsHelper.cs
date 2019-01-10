cd159ec602561b48a88c1aced0142deb3fee8420
Jan Sviland <jan@sviland.org>
2019-01-10 11:23:12 +0100

    four unit tests for getting log file from argument helper

--- test/XUnitTest/UnitTestArgumentsHelper.cs
+++ test/XUnitTest/UnitTestArgumentsHelper.cs
@@ -20,6 +20,29 @@ namespace XUnitTest
         {
             ArgumentsHelper argHelper = new ArgumentsHelper();
 
+            string[] input = new string[] { };
+
+            // return default on null
+            Assert.Equal("fail2ban.log", argHelper.GetInputFile(input));
+        }
+
+        [Fact]
+        public void TestGetLogFile3()
+        {
+            ArgumentsHelper argHelper = new ArgumentsHelper();
+
+            string[] input = new string[] { "-o", "output.txt" };
+
+            // return default on null
+            Assert.Equal("fail2ban.log", argHelper.GetInputFile(input));
+        }
+
+
+        [Fact]
+        public void TestGetLogFile4()
+        {
+            ArgumentsHelper argHelper = new ArgumentsHelper();
+
             string[] input = new string[] { "-r", "test.log" };
 
             Assert.Equal("test.log", argHelper.GetInputFile(input));
