542d3850d9ff4c1f49a2f44a2b24ed13fce3fb55
Jan Sviland <jan@sviland.org>
2019-01-10 12:17:23 +0100

    more test cases 7/10 pass

--- test/XUnitTest/UnitTestArgumentsHelper.cs
+++ test/XUnitTest/UnitTestArgumentsHelper.cs
@@ -48,5 +48,64 @@ namespace XUnitTest
             Assert.Equal("test.log", argHelper.GetInputFile(input));
         }
 
+        [Fact]
+        public void TestGetLogFile5()
+        {
+            ArgumentsHelper argHelper = new ArgumentsHelper();
+
+            string[] input = new string[] { "-r", "logfile.txt", "-o", "host.deny", "-i", "123.123.123.123" };
+
+            Assert.Equal("logfile.txt", argHelper.GetInputFile(input));
+        }
+
+        [Fact]
+        public void TestGetLogFile6()
+        {
+            ArgumentsHelper argHelper = new ArgumentsHelper();
+
+            string[] input = new string[] { "--read", "logfile.txt", "-o", "host.deny", "-i", "123.123.123.123" };
+
+            Assert.Equal("logfile.txt", argHelper.GetInputFile(input));
+        }
+
+        [Fact]
+        public void TestGetLogFile7()
+        {
+            ArgumentsHelper argHelper = new ArgumentsHelper();
+
+            string[] input = new string[] { "-o", "host.deny", "--read", "logfile.txt", "-i", "123.123.123.123" };
+
+            Assert.Equal("logfile.txt", argHelper.GetInputFile(input));
+        }
+        [Fact]
+        public void TestGetLogFile8()
+        {
+            ArgumentsHelper argHelper = new ArgumentsHelper();
+
+            string[] input = new string[] { "-o", "host.deny", "-i", "123.123.123.123", "--read", "logfile.txt" };
+
+            Assert.Equal("logfile.txt", argHelper.GetInputFile(input));
+        }
+
+        [Fact]
+        public void TestGetLogFile9()
+        {
+            ArgumentsHelper argHelper = new ArgumentsHelper();
+
+            string[] input = new string[] { "-o", "host.deny", "-i", "123.123.123.123", "--read", };
+
+            Assert.Equal("fail2ban.log", argHelper.GetInputFile(input));
+        }
+
+        [Fact]
+        public void TestGetLogFile10()
+        {
+            ArgumentsHelper argHelper = new ArgumentsHelper();
+
+            string[] input = new string[] { "-o", "host.deny", "-i", "123.123.123.123", "-r" };
+
+            Assert.Equal("fail2ban.log", argHelper.GetInputFile(input));
+        }
+
     }
 }
